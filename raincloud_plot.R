bf72293b4d9f14da78a6648da1b6a6dfba0ae464
camilasbraz <camilabraz03@gmail.com>
2023-10-21 13:57:55 -0300

    fix files

--- raincloud_plot.R
+++ raincloud_plot.R
@@ -3,7 +3,7 @@ library(tidyquant)
 library(ggdist)
 library(ggthemes)
 library(tidyverse)
-pal <- c("#FF8C00", "#A034F0", "#159090")
+
 
 
 setwd("~/github/0raincloud-plot")
@@ -28,26 +28,22 @@ df_precipitacao <- dados %>%
                                  6 + min(ano_num), "-", floor((ano_num - min(ano_num)) / 6) * 6 + min(ano_num) + 4)) %>% 
   group_by(ano, mes,periodo_6_anos) %>%
   summarize(mean_precipitation = mean(precipitacao_diaria_mm, na.rm = TRUE))
-# Defina a fonte Roboto
-theme_set(theme_minimal(base_family = "Roboto"))
 
+pal <- c("#00b4d8", "#0077b6", "#023e8a")
 df_precipitacao %>%
   ggplot(aes(x = factor(periodo_6_anos), y = mean_precipitation, fill = factor(periodo_6_anos))) +
-  # add half-violin from {ggdist} package
   stat_halfeye(
-    # adjust bandwidth
     adjust = 0.5,
-    # move to the right
     justification = -0.2,
-    # remove the slub interval
     .width = 0,
     point_colour = NA,
     alpha = 0.8
   ) +
   geom_boxplot(
     aes(color = periodo_6_anos,
-        color = after_scale(darken(color, .1, space = "HLS")),
-        fill = after_scale(desaturate(lighten(color, .8), .4))),
+        # color = after_scale(darken(color, .1, space = "HLS")),
+        #fill = after_scale(desaturate(lighten(color, .8), .4))),
+        fill =  periodo_6_anos),
     width = .2, 
     outlier.shape = NA,
     outlier.color = NA,
@@ -58,25 +54,16 @@ df_precipitacao %>%
     fun = "median",
     aes(label = round(..y.., 2),
         color = periodo_6_anos,
-        color = after_scale(darken(color, .1, space = "HLS"))),
-    family = "Roboto Mono",
+        #color = after_scale(darken(color, .1, space = "HLS"))
+        ),
+    family = "Roboto",
     fontface = "bold",
     size = 4.5,
-    #vjust = -3.5
+    hjust = -0.2,
   ) +
-
-  # geom_boxplot(
-  # width = 0.12,
-    # removing outliers
-  # outlier.color = NA,
-  # alpha = 0.5
-  #) +
   stat_dots(
-    # ploting on left side
     side = "left",
-    # adjusting position
     justification = 1.1,
-    # adjust grouping (binning) of observations
     binwidth = 0.25,
     color = "transparent"
   ) +
@@ -84,10 +71,10 @@ df_precipitacao %>%
   scale_color_manual(values = pal) +
   scale_fill_manual(values = pal)+
   labs(
-    title = "Precipitação em BH com RainCloud Plot",
-    x = "Período",
-    y = "Precipitacao mensal",
-    fill = "Período"
+    title = "",
+    x = "",
+    y = "",
+    fill = ""
   ) +
   coord_flip(xlim = c(1.2, NA), clip = "off") +
   scale_y_continuous(
@@ -95,59 +82,23 @@ df_precipitacao %>%
     breaks = seq(0, 30, by = 5),
     expand = c(.001, .001)
   ) +
-  theme(panel.background = element_rect(fill = "#f2f2f2"),
-                      #panel.grid.major = element_blank(),
-                      panel.grid.minor = element_blank(),
-                       #panel.background = element_blank()
-                       )
+  theme_minimal(base_family = "Roboto", base_size = 15) +
+  theme(
+    panel.grid.minor = element_blank(),
+    panel.grid.major.y = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text.x = element_text(family = "Roboto"),
+    axis.text.y = element_text(
+      #color = darken(pal, .1, space = "HLS"), 
+      size = 18
+    ),
+    axis.title.x = element_text(margin = margin(t = 10),
+                                size = 16),
+ 
+   plot.margin = unit(c(1.5, 1.5, 1.5, 1.5), "cm"),
+    legend.position="none",
+   # element_rect(fill = "#f2f2f2")
 
+  )
 
-df_precipitacao <- dados %>% 
-  na.omit(df) %>% 
-  group_by(Data.Medicao) %>%
-  summarize(mean_precipitation = mean(precipitacao_diaria_mm, na.rm = TRUE)) %>% 
-  mutate(ano = lubridate::year(Data.Medicao),
-         ano_num = as.numeric(ano),
-         periodo_6_anos = paste0(floor((ano_num - min(ano_num)) / 6) *
-                                   6 + min(ano_num), "-", floor((ano_num - min(ano_num)) / 6) * 6 + min(ano_num) + 4))
-
-
-df_precipitacao %>%
-  ggplot(aes(x = factor(periodo_6_anos), y = mean_precipitation, fill = factor(periodo_6_anos))) +
-  
-  # add half-violin from {ggdist} package
-  stat_halfeye(
-    # adjust bandwidth
-    adjust = 0.5,
-    # move to the right
-    justification = -0.2,
-    # remove the slub interval
-    .width = 0,
-    point_colour = NA
-  ) +
-  geom_boxplot(
-    width = 0.12,
-    # removing outliers
-    outlier.color = NA,
-    alpha = 0.5
-  ) +
-  stat_dots(
-    # ploting on left side
-    side = "left",
-    # adjusting position
-    justification = 1.1,
-    # adjust grouping (binning) of observations
-    binwidth = 0.15,
-    color = "red"
-  ) +
-  # Themes and Labels
-  scale_fill_tq() +
-  theme_tq() +
-  labs(
-    title = "Precipitação em BH com RainCloud Plot",
-    x = "Período",
-    y = "Precipitacao mensal",
-    fill = "Período"
-  ) +
-  coord_flip()
 
